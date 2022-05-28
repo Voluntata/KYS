79215af4afafa67069237695ff38204dce38043b
Voluntata <voluntata@gmail.com>
2022-05-28 18:43:27 +0200

    filter

--- src/app/mapbox/mapbox.component.ts
+++ src/app/mapbox/mapbox.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild, ElementRef, Input, ViewChildren, QueryList, Renderer2} from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef, Input, ViewChildren, QueryList, Renderer2 } from '@angular/core';
 import Mapboxgl, { LngLatBounds, NavigationControl, GeolocateControl, Map, Popup, Marker } from "mapbox-gl"
 import { environment } from 'src/environments/environment';
 import { MapBoxModel } from './mapboxinterface';
@@ -11,12 +11,14 @@ import { MapBoxModel } from './mapboxinterface';
 
 
 export class MapboxComponent implements AfterViewInit {
-  navBarOpen:boolean = false;
   @ViewChild("mapDiv")
   mapDivElement!: ElementRef;
 
   @ViewChildren('mapboxgl-marker') markers!: QueryList<any>;
-
+  safeResultsToPrintOnMap: any[] = [];
+  dangerResultsToPrintOnMap: any[] = [];
+  helpResultsToPrintOnMap: any[] = [];
+  friendResultsToPrintOnMap: any[] = []
   //@Input() filteredResultsToPrintOnMap!: MapBoxModel[];
   filteredResultsToPrintOnMap = [
     {
@@ -132,122 +134,158 @@ export class MapboxComponent implements AfterViewInit {
     ],
   }
 
-  constructor( private renderer: Renderer2) {
-   // console.log('result'+this.filteredResultsToPrintOnMap)
-   }
+  constructor() {
 
-
-ngAfterViewInit(): void {
-  // Generate map with basic config
-  this.generateMap();
-  // Depending on if the user accepts to share their location, center the map into the user, or into the default location (IT Academy)
-  this.getUsersLocation();
-
-
-  this.filteredResultsToPrintOnMap.forEach((result) => {
-    // Create a marker for each result and add it to the map
-    if(result.type === "safe"){
-    this.createANewMarker("green", result);}
-    if(result.type === "danger"){
-      this.createANewMarker("red", result);}
-      if(result.type === "amigo"){
-        this.createANewMarker("yellow", result);}
-        if(result.type === "help"){
-          this.createANewMarker("purple", result);}
-  console.log(this.currentMarkers)
-  });
-
-  //  this.currentMarkers.forEach(marker => {this.element = marker.getElement();
-  //  this.element.className = "mapboxgl-marker-1";
-  // console.log(this.element)})
-
-}
-
-// ngOnChanges() {
-//   this.filteredResultsToPrintOnMap.forEach((result) => {
-//     // Create a marker for each result and add it to the map
-//     this.createANewMarker("green", result);
-//     console.log(result)
-
-//   });
-// }
-
-ngOnDestroy() {
-  this.currentMarkers.forEach(marker => marker.remove());
-}
-
-generateMap() {
-  Mapboxgl.accessToken = environment.MAPBOX_TOKEN;
-  this.map = new Map({
-    container: this.mapDivElement.nativeElement,
-    style: environment.MAPBOX_STYLE,
-   // center: [this.MAPBOX_INIT_LOCATION.address[0].location.x, this.MAPBOX_INIT_LOCATION.address[0].location.y], // starting center so it doesn't start from Germany
-    zoom: environment.MAPBOX_ZOOM
-  });
-
-  this.map.addControl(new NavigationControl());
-
-  // Add geolocate control to the map.
-  this.map.addControl(
-    new GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true,
-    })
-  );
-}
-
-// Function to create a single marker (with the marker's colour and the business (or user's coords) as parameters)
-createANewMarker(markerColor: string, business?: MapBoxModel, coord?: GeolocationCoordinates): void {
-
-  // Create a popup with the business's basic information
-  const popup = new Popup().setHTML(
-    `<b>${business?.name}</b> </br> ${business?.address[0].street_name} , ${business?.address[0].street_number}`
-  );
-
-  if (coord) { // If user has accepted to share their location
-
-    const newIndividualMarker = new Marker({ color: markerColor })
-      .setLngLat([coord.longitude, coord.latitude])
-      .addTo(this.map);
-    this.currentMarkers.push(newIndividualMarker);
-  } else { // If user hasn't accepted to share their location OR when iterating through the filteredResultsToPrintOnMap array.
-    const newIndividualMarker = new Marker({ color: markerColor })
-      .setLngLat([business!.address[0].location.x, business!.address[0].location.y])
-      .setPopup(popup)
-      .addTo(this.map);
-    this.currentMarkers.push(newIndividualMarker);
   }
 
-  // MAP LÍMITS
-  // Initial point 0
-  const bounds = new LngLatBounds();
 
-  // Add all the markers to the map's bounds.
-  this.currentMarkers.forEach(marker =>
-    bounds.extend(marker.getLngLat()));
-  console.log(bounds)
-  // Adjust the zoom to see all the existing markers
-  this.map.fitBounds(bounds, {
-    padding: 75
-  })
-}
+  ngAfterViewInit(): void {
+    // Generate map with basic config
+    this.generateMap();
+    // Depending on if the user accepts to share their location, center the map into the user, or into the default location (IT Academy)
+    this.getUsersLocation();
 
-getUsersLocation(): void {
-  navigator.geolocation.getCurrentPosition(
-    // Success callback function (if user has accepted to share their location)
-    (pos) => {
-      // this.map.flyTo({ center: [pos.coords.longitude, pos.coords.latitude], zoom: 11 })
-      this.createANewMarker("blue",undefined, pos.coords);
-      console.log(pos.coords)
-    },
-    // Error callback function (if user hasn't accepted to share their location)
-    () => {
-      // this.map.flyTo(
-      //  { center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], zoom: 11 })
-     this.createANewMarker("blue", this.MAPBOX_INIT_LOCATION);
+
+      this.filteredResultsToPrintOnMap.forEach((result) => {
+    // Create a marker for each result and add it to the map
+      if (result.type === "safe") {
+        this.createANewMarker("green", result);
+      }
+      if (result.type === "danger") {
+        this.createANewMarker("red", result);
+      }
+      if (result.type === "amigo") {
+        this.createANewMarker("yellow", result);
+      }
+      if (result.type === "help") {
+        this.createANewMarker("purple", result);
+      }
+    //  console.log(this.currentMarkers)
+    });
+
+    //  this.currentMarkers.forEach(marker => {this.element = marker.getElement();
+    //  this.element.className = "mapboxgl-marker-1";
+    // console.log(this.element)})
+
+  }
+
+  // ngOnChanges() {
+  //   this.filteredResultsToPrintOnMap.forEach((result) => {
+  //     // Create a marker for each result and add it to the map
+  //     this.createANewMarker("green", result);
+  //     console.log(result)
+
+  //   });
+  // }
+ showSafe(){
+  this.currentMarkers.forEach(marker => marker.remove());
+  this.safeResultsToPrintOnMap =  this.filteredResultsToPrintOnMap.filter(el =>
+     el.type === "safe");
+     console.log(this.filteredResultsToPrintOnMap)
+     this.safeResultsToPrintOnMap.forEach((result) => {
+      // Create a marker for each result and add it to the map
+          this.createANewMarker("green", result);
+        })
+ }
+
+ showDanger(){
+  this.currentMarkers.forEach(marker => marker.remove());
+  this.dangerResultsToPrintOnMap =  this.filteredResultsToPrintOnMap.filter(el =>
+     el.type === "danger");
+     console.log(this.filteredResultsToPrintOnMap)
+     this.dangerResultsToPrintOnMap.forEach((result) => {
+      // Create a marker for each result and add it to the map
+          this.createANewMarker("red", result);
+        })
+ }
+
+ showHelp(){
+  this.currentMarkers.forEach(marker => marker.remove());
+  this.helpResultsToPrintOnMap =  this.filteredResultsToPrintOnMap.filter(el =>
+     el.type === "help");
+     console.log(this.filteredResultsToPrintOnMap)
+     this.helpResultsToPrintOnMap.forEach((result) => {
+      // Create a marker for each result and add it to the map
+          this.createANewMarker("purple", result);
+        })
+ }
+
+  ngOnDestroy() {
+    this.currentMarkers.forEach(marker => marker.remove());
+  }
+
+  generateMap() {
+    Mapboxgl.accessToken = environment.MAPBOX_TOKEN;
+    this.map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: environment.MAPBOX_STYLE,
+      // center: [this.MAPBOX_INIT_LOCATION.address[0].location.x, this.MAPBOX_INIT_LOCATION.address[0].location.y], // starting center so it doesn't start from Germany
+      zoom: environment.MAPBOX_ZOOM
+    });
+
+    this.map.addControl(new NavigationControl());
+
+    // Add geolocate control to the map.
+    this.map.addControl(
+      new GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
+    );
+  }
+
+  // Function to create a single marker (with the marker's colour and the business (or user's coords) as parameters)
+  createANewMarker(markerColor: string, business?: MapBoxModel, coord?: GeolocationCoordinates): void {
+
+    // Create a popup with the business's basic information
+    const popup = new Popup().setHTML(
+      `<b>${business?.name}</b> </br> ${business?.address[0].street_name} , ${business?.address[0].street_number}`
+    );
+
+    if (coord) { // If user has accepted to share their location
+
+      const newIndividualMarker = new Marker({ color: markerColor })
+        .setLngLat([coord.longitude, coord.latitude])
+        .addTo(this.map);
+      this.currentMarkers.push(newIndividualMarker);
+    } else { // If user hasn't accepted to share their location OR when iterating through the filteredResultsToPrintOnMap array.
+      const newIndividualMarker = new Marker({ color: markerColor })
+        .setLngLat([business!.address[0].location.x, business!.address[0].location.y])
+        .setPopup(popup)
+        .addTo(this.map);
+      this.currentMarkers.push(newIndividualMarker);
     }
-  );
-}
+
+    // MAP LÍMITS
+    // Initial point 0
+    const bounds = new LngLatBounds();
+
+    // Add all the markers to the map's bounds.
+    this.currentMarkers.forEach(marker =>
+      bounds.extend(marker.getLngLat()));
+    //console.log(bounds)
+    // Adjust the zoom to see all the existing markers
+    this.map.fitBounds(bounds, {
+      padding: 75
+    })
+  }
+
+  getUsersLocation(): void {
+    navigator.geolocation.getCurrentPosition(
+      // Success callback function (if user has accepted to share their location)
+      (pos) => {
+        // this.map.flyTo({ center: [pos.coords.longitude, pos.coords.latitude], zoom: 11 })
+        this.createANewMarker("blue", undefined, pos.coords);
+       // console.log(pos.coords)
+      },
+      // Error callback function (if user hasn't accepted to share their location)
+      () => {
+        // this.map.flyTo(
+        //  { center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], zoom: 11 })
+        this.createANewMarker("blue", this.MAPBOX_INIT_LOCATION);
+      }
+    );
+  }
 }
